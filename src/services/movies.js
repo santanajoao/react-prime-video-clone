515b5f592c807dc408d9f282b045ae9685cb14fc
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-03-28 23:12:28 -0300

    feat: create fetchByGenre function and refactor fetchPopular

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,7 +1,7 @@
-export async function fetchPopularMovies() {
-  const response = await fetch(
-    'https://api.themoviedb.org/3/trending/all/week?api_key=8427e7de4210382e41fa0812d7eaf4c7&include_image_language=pt-BR',
-  );
+const API_KEY = '8427e7de4210382e41fa0812d7eaf4c7';
+
+async function fetchMovie(endpoint) {
+  const response = await fetch(endpoint);
 
   if (!response.ok) {
     throw new Error(response.statusText);
@@ -11,4 +11,99 @@ export async function fetchPopularMovies() {
   return results;
 }
 
-// https://api.themoviedb.org/3/discover/movie?api_key=8427e7de4210382e41fa0812d7eaf4c7&language=pt-BR&with_genres=18&include_image_language=pt-BR&sort_by=popularity.desc
+export async function fetchPopularMovies() {
+  const results = await fetchMovie(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&include_image_language=pt-BR`,
+  );
+  return results;
+}
+
+export async function fetchMoviesByGenres(genreId) {
+  const results = await fetchMovie(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=pt-BR&with_genres=${genreId}&include_image_language=pt-BR&sort_by=popularity.desc`,
+  );
+  return results;
+}
+
+// https://api.themoviedb.org/3/discover/movie?api_key=8427e7de4210382e41fa0812d7eaf4c7&language=pt-BR&with_genres=28&include_image_language=pt-BR&sort_by=popularity.desc
+
+// {
+//   "genres": [
+//     {
+//       "id": 28,
+//       "name": "Ação"
+//     },
+//     {
+//       "id": 12,
+//       "name": "Aventura"
+//     },
+//     {
+//       "id": 16,
+//       "name": "Animação"
+//     },
+//     {
+//       "id": 35,
+//       "name": "Comédia"
+//     },
+//     {
+//       "id": 80,
+//       "name": "Crime"
+//     },
+//     {
+//       "id": 99,
+//       "name": "Documentário"
+//     },
+//     {
+//       "id": 18,
+//       "name": "Drama"
+//     },
+//     {
+//       "id": 10751,
+//       "name": "Família"
+//     },
+//     {
+//       "id": 14,
+//       "name": "Fantasia"
+//     },
+//     {
+//       "id": 36,
+//       "name": "História"
+//     },
+//     {
+//       "id": 27,
+//       "name": "Terror"
+//     },
+//     {
+//       "id": 10402,
+//       "name": "Música"
+//     },
+//     {
+//       "id": 9648,
+//       "name": "Mistério"
+//     },
+//     {
+//       "id": 10749,
+//       "name": "Romance"
+//     },
+//     {
+//       "id": 878,
+//       "name": "Ficção científica"
+//     },
+//     {
+//       "id": 10770,
+//       "name": "Cinema TV"
+//     },
+//     {
+//       "id": 53,
+//       "name": "Thriller"
+//     },
+//     {
+//       "id": 10752,
+//       "name": "Guerra"
+//     },
+//     {
+//       "id": 37,
+//       "name": "Faroeste"
+//     }
+//   ]
+// }

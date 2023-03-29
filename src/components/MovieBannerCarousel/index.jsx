ab355064749d43ea31d8f1bbcc415c0f0dd9f851
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-03-28 21:11:31 -0300

    chore: remove unused inports, styles and changes api endpoint

--- src/components/MovieBannerCarousel/index.jsx
+++ src/components/MovieBannerCarousel/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SlArrowLeft, SlArrowRight } from 'react-icons/sl';
 import useIndexSelection from '../../hooks/useIndexSelection';
 import { fetchPopularMovies } from '../../services/movies';
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,6 +1,6 @@
 export async function fetchPopularMovies() {
   const response = await fetch(
-    'https://api.themoviedb.org/3/discover/movie?api_key=8427e7de4210382e41fa0812d7eaf4c7&language=pt-BR&region=BR&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate',
+    'https://api.themoviedb.org/3/trending/all/week?api_key=8427e7de4210382e41fa0812d7eaf4c7&include_image_language=pt-BR',
   );
 
   if (!response.ok) {
@@ -10,3 +10,5 @@ export async function fetchPopularMovies() {
   const { results } = await response.json();
   return results;
 }
+
+// https://api.themoviedb.org/3/discover/movie?api_key=8427e7de4210382e41fa0812d7eaf4c7&language=pt-BR&with_genres=18&include_image_language=pt-BR&sort_by=popularity.desc

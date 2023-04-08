89c027488cd6d39f161983339bad60fcc3407897
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-04-07 23:19:35 -0300

    fix: ancor clickable area

--- src/components/CategorySection/MovieCard/index.jsx
+++ src/components/CategorySection/MovieCard/index.jsx
@@ -11,8 +11,8 @@ export default function MovieCard({
   onFocus,
 }) {
   return (
-    <a href="#" onFocus={onFocus} className={styles.movie_ancor}>
-      <div className={styles.movie_card}>
+    <div className={styles.movie_card}>
+      <a href="#" onFocus={onFocus} className={styles.movie_ancor}>
         <div className={styles.tag_wrapper}>
           <div className={styles.prime_tag}>
             <span aria-hidden="true">prime</span>
@@ -25,30 +25,29 @@ export default function MovieCard({
           className={styles.movie_banner}
           alt={title}
         />
+      </a>
+      <div className={styles.card_bottom}>
+        <div className={styles.card_interactions}>
+          <button type="button" className={styles.button_ancor}>
+            Assistir com o prime
+          </button>
 
-        <div className={styles.card_bottom}>
-          <div className={styles.card_interactions}>
-            <button type="button" className={styles.button_ancor}>
-              Assistir com o prime
-            </button>
+          <TextLessButton
+            title="Adicionar aos favoritos"
+            className={styles.favorite_button}
+            textTip="Adicionar -nome- aos favoritos"
+          >
+            <IoMdAdd />
+          </TextLessButton>
+        </div>
 
-            <TextLessButton
-              title="Adicionar aos favoritos"
-              className={styles.favorite_button}
-              textTip="Adicionar -nome- aos favoritos"
-            >
-              <IoMdAdd />
-            </TextLessButton>
-          </div>
+        <div>
+          <h2 className={styles.card_title}>{title}</h2>
+          <p className={styles.card_description}>{description}</p>
 
-          <div>
-            <h2 className={styles.card_title}>{title}</h2>
-            <p className={styles.card_description}>{description}</p>
-
-            <span className={styles.year}>{releaseYear}</span>
-          </div>
+          <span className={styles.year}>{releaseYear}</span>
         </div>
       </div>
-    </a>
+    </div>
   );
 }
